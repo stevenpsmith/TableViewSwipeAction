190f4f01ee59f62260ffbb7ecaef41a10054b3ff
Steve Smith <ssmith@chariotsolutions.com>
2017-08-02 13:42:00 -0400

    Initial Commit

---
+++ TableViewSwipe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewSwipe
+//
+//  Created by Steven Smith on 8/2/17.
+//  Copyright © 2017 Steven Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
